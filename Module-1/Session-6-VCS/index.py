771afc5193d7d4ee105c8c43741c274ed4dfb3fc
Uday Verma <uday.verma@binarysemantics.com>
2026-04-13 21:55:43 +0530

    find all even numbers from a given list

---
+++ Module-1/Session-6-VCS/index.py
@@ -0,0 +1,6 @@
+# Program to print all even numbers from a given list
+numbers = [10, 25, 34, 47, 52, 63, 78, 81, 92, 100]
+
+even_nums = [num for num in numbers if num % 2 == 0]
+print("Even numbers from the list:", even_nums)
+
